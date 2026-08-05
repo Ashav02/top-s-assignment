8f561b395d809ac2bea017be372e4bee0c350883
Ashav Patel <aashav2112@gmail.com>
2026-08-05 14:40:44 +0000

    session-2

---
+++ session-2/single-line and triple quotes.py
@@ -0,0 +1,18 @@
+#Spotify playlist recommendation system might work
+
+"""Spotify recommends playlists based on listening history,
+favorite artists, genres, and liked songs.
+This is a demonstration of comments in Python."""
+
+# Store user information
+user_name = "Ashav Patel"
+favorite_genre = "All Generation"
+favorite_artist = "Arijit Singh, Shreya Ghosal, ect.."
+
+# Display user details
+print("User Name:", user_name)
+print("Favorite Genre:", favorite_genre)
+print("Favorite Artist:", favorite_artist)
+
+# Display recommendation
+print("Recommended Playlist: Top Pop Hits")
